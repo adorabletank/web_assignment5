4860ea92eddba957efe9a8a2ea0036c73ec1cf5c
Your Name <you@example.com>
2022-11-08 02:55:18 -0500

    test server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("Hello World! yeahhhhh");
 });
 
+app.get("/about", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/Blog.html"));
+});
+
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
 
