e8cc9a0cfa18c7185d37c4550fc813eccd0bab3f
Your Name <you@example.com>
2022-11-10 17:20:40 -0500

    new

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var HTTP_PORT = process.env.PORT || 8080;
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World! yeahhhhh");
+    res.send("Hello World!/ soup");
 });
 
 app.get("/about", function(req, res){
-    res.sendFile(path.join(__dirname, "\\views\\Blog.html"));
+    res.sendFile(path.join(__filename, ".\\Blog.html"));
 });
 
 // setup http server to listen on HTTP_PORT
