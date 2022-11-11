95627482e82f924d60ce52382c181b7fa03c3f18
Your Name <you@example.com>
2022-11-10 22:13:22 -0500

    new

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ var path = require("path");
 
 var HTTP_PORT = process.env.PORT || 8080;
 
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+
+}
+
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!/ soup");
+    res.send("Hello World!!!/ soup");
 });
 
 app.get("/about", function(req, res){
-    res.sendFile(path.join(__filename, ".\\Blog.html"));
+    res.sendFile(path.join(__dirname, "\\view\\Blog.html"));
 });
 
+
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT, onHttpStart);
 
