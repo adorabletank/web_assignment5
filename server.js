21b6ed5776812ca13265a3cdbd83cf9df851a664
Your Name <you@example.com>
2022-11-11 23:08:13 -0500

    new commit

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
+const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
 var path = require("path");
 
 var HTTP_PORT = process.env.PORT || 8080;
 
-function onHttpStart() {
-    console.log("Express http server listening on: " + HTTP_PORT);
 
-}
+
 
 // setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.send("Hello World!!!/ soup");
+app.use(express.static('public'));
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, ".\\view\\Home.html"));
 });
 
-app.get("/about", function(req, res){
-    res.sendFile(path.join(__dirname, "\\view\\Blog.html"));
+app.get("/blog", function(req, res){
+    res.sendFile(path.join(__dirname, ".\\view\\Blog.html"));
 });
 
+app.get("/article", function(req, res){
+    res.sendFile(path.join(__dirname, ".\\view\\article.html"));
+});
+
+app.get("/registration", function(req, res){
+    res.sendFile(path.join(__dirname, ".\\view\\registration.html"));
+});
+
+app.get("/login", function(req, res){
+    res.sendFile(path.join(__dirname, ".\\view\\login.html"));
+});
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+app.listen(HTTP_PORT);
 
